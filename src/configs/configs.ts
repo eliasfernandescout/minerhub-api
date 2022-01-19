9657b8f5b5858f614aaa7add4a01216b5b65fe58
Elias Fernandes <eliasfernandescout@gmail.com>
2022-01-18 22:27:16 -0300

    chore: ensure its possible do make a get request with axios

---
+++ src/configs/configs.ts
@@ -0,0 +1,4 @@
+export const urlByCurrencyAndUser = (currency: string, user: string) =>
+    `https://https://api.f2pool.com/${currency}/${user}`;
+
+// export { urlByCurrencyAndUser };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,36 @@
-import exppress, { response } from "express";
+import cors from 'cors';
+import exppress, { response } from 'express';
+
+const axios = require('axios').default;
 
 const app = exppress();
 app.use(exppress.json());
 
-app.get("/", (request, response) => {
-    response.send({ name: "hello world" });
-    // console.log({ name: "hello world" });
+app.use(
+    cors({
+        origin: '*',
+    }),
+);
+
+const instance = axios.create({
+    baseURL: 'https://api.f2pool.com/bitcoin/mattei2005',
+    timeout: 1000,
+    // headers: {'X-Custom-Header': 'foobar'}
+});
+
+
+
+app.get('/minerhubdata/:currency/:user', async (request, response) => {
+    const { currency, user } = request.params;
+    try {
+        const { data } = await axios.get(`https://api.f2pool.com/${currency}/${user}`);
+        console.log(data)
+        response.send(data)
+    } catch (error) {
+        console.log(error)
+    }
 });
 
 app.listen(5002, () => {
-    console.log("SERVER STARTED ON PORT 5002");
+    console.log('SERVER STARTED ON PORT 5002');
 });
